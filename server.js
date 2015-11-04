b57f175c132f4477b89e6c60c5d087d1f15073f9
Tim <hosweetim@gmail.com>
2015-11-04 23:46:23 +0900

    added route

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-'use stict';
+'use strict';
 
 let express = require('express');
+let bodyParser = require('body-parser');
+let expressHandleBar = require('express-handlebars');
+
+let config = require('./config/all');
 
 let app = express();
 
-app.set('port') = process.env.PORT || 3000;
+app.engine('handlebars', expressHandleBar({
+    defaultLayout: 'main'
+}));
 
+app.set('view engine', handlebars);
 
-const server = app.listen(app.get('port'), () => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+const server = app.listen(config.port, () => {
     const port = server.address().port;
 
     console.log(`Server started in port ${port}`);
