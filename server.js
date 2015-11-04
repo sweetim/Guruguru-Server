af61f313fe829c3697899fd225672bcf5f04df0e
Tim <hosweetim@gmail.com>
2015-11-05 01:43:05 +0900

    added homepage

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.engine('handlebars', expressHandleBar({
 
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
